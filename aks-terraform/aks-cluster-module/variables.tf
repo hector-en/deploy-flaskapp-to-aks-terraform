3e5d9cd2de4e129965798f3b8def0994e4074222
Hector Edu Nseng <aicore.temp@outlook.com>
2024-01-25 22:43:56 +0000

    devops-02: Define input variables for Terraform modules

---
+++ aks-terraform/aks-cluster-module/variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" {
+  description = "Represents the Resource Group where AKS cluster resources will be deployed."
+  type        = string
+  default     = "aks-cluster-rg"
+}
