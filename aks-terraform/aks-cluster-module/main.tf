1a97f5474196f59714ec119ae7075e2425488093
Hector Edu Nseng <aicore.temp@outlook.com>
2024-01-26 21:32:21 +0000

    devops-03 (update): Adds comments and a verbose environment.

--- aks-terraform/aks-cluster-module/main.tf
+++ aks-terraform/aks-cluster-module/main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_resource_group" "rg" {
+  # This resource block creates the resource group for AKS cluster resources in Azure.
   name     = var.resource_group_name
   location = var.location
 }
--- aks-terraform/networking-module/main.tf
+++ aks-terraform/networking-module/main.tf
@@ -1,8 +1,10 @@
 resource "azurerm_resource_group" "rg" {
+# This resource block creates the resource group for networking resources in Azure.
   name     = var.resource_group_name
   location = var.location
 }
 resource "azurerm_virtual_network" "vnet" {
+# This resource block creates a Virtual Network in Azure.
   name                = "aks-vnet"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -10,6 +12,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "control_plane_subnet" {
+# This resource block creates a subnet for the control plane of the AKS cluster.
   name                 = "control-plane-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
@@ -17,6 +20,7 @@ resource "azurerm_subnet" "control_plane_subnet" {
 }
 
 resource "azurerm_subnet" "worker_node_subnet" {
+# This resource block creates a subnet for the worker nodes of the AKS cluster.
   name                 = "worker-node-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
@@ -24,11 +28,13 @@ resource "azurerm_subnet" "worker_node_subnet" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
+# This resource block creates a Network Security Group for controlling access to the AKS cluster.
   name                = "aks-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
+# This security rule allows inbound traffic to the kube-apiserver from a specific IP address.
     name                       = "kube-apiserver-rule"
     priority                   = 1001
     direction                  = "Inbound"
@@ -41,6 +47,7 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
+# This security rule allows inbound SSH traffic from a specific IP address.
     name                       = "ssh-rule"
     priority                   = 1002
     direction                  = "Inbound"
